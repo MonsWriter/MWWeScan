6ccd3868b2eb16485bcb57ebfc2711db9e89b04c
MonsWriter <monswriter@gmail.com>
2025-06-12 13:46:09 +0500

    Update QuadrilateralView.swift

--- Sources/WeScan/Common/QuadrilateralView.swift
+++ Sources/WeScan/Common/QuadrilateralView.swift
@@ -81,6 +81,7 @@ public final class QuadrilateralView: UIView {
             bottomRightCornerView.strokeColor = strokeColor
             bottomLeftCornerView.strokeColor = strokeColor
             updateCornerStrokeWidths()
+            updateCornerMagnificationScales()
         }
     }
 
